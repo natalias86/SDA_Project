2782b937ed8f095eb05869a79dc5fa2b225b94fc
Natalia Szulc <nataliaszulc01@gmail.com>
2019-07-13 21:19:16 +0200

    first commit

---
+++ src/main/java/pl/sda/sda_biuro_podrozy/SdaBiuroPodrozyApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.sda_biuro_podrozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaBiuroPodrozyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaBiuroPodrozyApplication.class, args);
+    }
+
+}
